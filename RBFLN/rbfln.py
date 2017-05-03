6f89b8cccd4f8f0530d551cbed0e681b91fcec18
juan_sca <juansca1229@gmail.com>
2017-05-02 22:30:05 -0300

    rbfln docstrings

--- RBFLN/rbfln.py
+++ RBFLN/rbfln.py
@@ -10,22 +10,37 @@ class RBFLN(object):
         pass
 
     def _update_weights(self):
+        """Update weights using gradient descent."""
         pass
 
     def _update_center_vectors(self):
+        """Update center vectors using gradient descent."""
         pass
 
     def _update_variance(self):
+        """Update variance value using gradient descent."""
         pass
 
     def _init_weights(self):
+        """Init linear and non-linear weights.
+
+        Select all weights randomly between -0.5 and 0.5."""
         pass
 
     def _init_variance(self):
+        """Compute initial values for variances."""
+        # (1/2) * (1/M) ** 1/N
         pass
 
     def _init_center_vectors(self):
+        """Init center vectors.
+
+        Let Q be the number of input feature vectors.
+        Initialize RBF center vectors by putting v(m) = x(m) if M <= Q, else
+        put v(q) = x(q) , q = 1,...,Q, and draw the remaining M - Q centers at
+        random in the feature space."""
         pass
 
     def _init_learning_rates(self):
+        """Init learning rates."""
         pass
