987f5113c88f669dea3c92216c64ac54468d71a9
juan_sca <juansca1229@gmail.com>
2017-05-02 23:20:17 -0300

    Fix naming conventions

--- RBFLN/rbfln.py
+++ RBFLN/rbfln.py
@@ -1,25 +1,25 @@
 class RBFLN(object):
 
-    def __init__(self, xs, ts, M, N, I=100, etas=None, variance=None):
+    def __init__(self, xs, ts, m, n, niter=100, etas=None, variance=None):
         """Create a Radial Basis Functional Link Network.
 
-        Create a RBFLN with N neurons in the input layer, M neurons in the
+        Create a RBFLN with n neurons in the input layer, m neurons in the
         hidden layer and 1 neuron in the output layer.
         The xs and ts parameters should have the same length.
-        The lengths of all the elements of xs should be equal to N.
+        The lengths of all the elements of xs should be equal to n.
         The lengths of all the elements of ts should be equal to 1.
 
         :param xs: input feature vectors used for training.
         :param ts: associated output target vectors used for training.
-        :param M: Number of neurons in the hidden layer.
-        :param N: Number of neurons in the input layer.
-        :param I: Number of iterations.
+        :param m: Number of neurons in the hidden layer.
+        :param n: Number of neurons in the input layer.
+        :param niter: Number of iterations.
         :param etas: Learning rates.
         :param variance: The initial variance of the RBF.
 
-        :type M: int
-        :type N: int
-        :type I: int
+        :type m: int
+        :type n: int
+        :type niter: int
         :type etas: list of int
         :type variance: float
         """
