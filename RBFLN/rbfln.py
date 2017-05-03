8c815b336cc553d7f0b014bd8bc9fe471751ba9a
juan_sca <juansca1229@gmail.com>
2017-05-02 21:37:59 -0300

    Add RBFLN skeleton

---
+++ RBFLN/rbfln.py
@@ -0,0 +1,31 @@
+class RBFLN(object):
+
+    def __init__(self):
+        pass
+
+    def _sum_sq_error(self, xs, ts):
+        pass
+
+    def total_sq_error(self, xs, ts):
+        pass
+
+    def _update_weights(self):
+        pass
+
+    def _update_center_vectors(self):
+        pass
+
+    def _update_variance(self):
+        pass
+
+    def _init_weights(self):
+        pass
+
+    def _init_variance(self):
+        pass
+
+    def _init_center_vectors(self):
+        pass
+
+    def _init_learning_rates(self):
+        pass
