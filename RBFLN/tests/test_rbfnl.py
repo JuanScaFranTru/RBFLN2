353cf66ccbc202f4b1e6544786fba96f99530d26
Francisco Trucco <franciscoctrucco@gmail.com>
2017-05-03 00:17:50 -0300

    Add unit tests

---
+++ RBFLN/tests/test_rbfnl.py
@@ -0,0 +1,37 @@
+from unittest import TestCase
+
+
+class TestRBFLN(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_sum_sq_error(self):
+        pass
+
+    def test_total_sq_error(self):
+        pass
+
+    def test_evaluate(self):
+        pass
+
+    def test_update_weights(self):
+        pass
+
+    def test_update_center_vectors(self):
+        pass
+
+    def test_update_variance(self):
+        pass
+
+    def test_init_weights(self):
+        pass
+
+    def test_init_variance(self):
+        pass
+
+    def test_init_center_vectors(self):
+        pass
+
+    def test_init_learning_rates(self):
+        pass
